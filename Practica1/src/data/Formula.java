979dbe1dfe29b09c810957048d659d220302f2d1
Patricia Lazaro <plazaro1994@gmail.com>
2016-01-03 13:36:44 +0100

    formula comentada
    Y asi, el paquete data quedo listo para entregar

--- Practica1/src/data/Formula.java
+++ Practica1/src/data/Formula.java
@@ -1,3 +1,8 @@
+/*
+ * Patricia Lazaro Tello (554309)
+ * Alejandro Royo Amondarain (560285)
+ */
+
 package data;
 
 import java.util.LinkedList;
@@ -5,23 +10,40 @@ import java.util.List;
 import java.util.Set;
 
 public class Formula {
+	/*
+	 * Clase que representa una formula en CNF
+	 */
 
 	private List<Clausula> clausulas;
 	private Set<Variable> vars;
 	
+	/**
+	 * Constructor
+	 * @param vars variables que utiliza la formula CNF
+	 */
 	public Formula(Set<Variable> vars){
 		clausulas = new LinkedList<Clausula>();
 		this.vars = vars;
 	}
 	
+	/**
+	 * @param c clausula a agregar a la formula
+	 */
 	public void addClausula(Clausula c){
 		clausulas.add(c);
 	}
 	
+	/**
+	 * @param c clausula que eliminar de la formula
+	 */
 	public void removeClausula(Clausula c){
 		clausulas.remove(c);
 	}
 	
+	/**
+	 * @return el valor de la formula CNF, que se calcula como
+	 * el operador AND sobre todas las clausulas de la formula
+	 */
 	public boolean getValor(){
 		boolean resultado = false;
 		if(clausulas.isEmpty()){
@@ -37,14 +59,25 @@ public class Formula {
 		return resultado;
 	}
 	
+	/**
+	 * @return el listado de clausulas de la formula
+	 */
 	public List<Clausula> getClausulas(){
 		return clausulas;
 	}
 	
+	/**
+	 * @return el conjunto de variables de la formula
+	 */
 	public Set<Variable> getVariables(){
 		return vars;
 	}
 	
+	/**
+	 * @param c clausula
+	 * @return true si la clausula esta contenida en la formula,
+	 * false en caso contrario
+	 */
 	public boolean contains(Clausula c){
 		if(c == null){
 			return false;
